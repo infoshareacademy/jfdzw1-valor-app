9e57451e70e6e14a6aeed125a9dc77700bf0526a
Krzysztof <lamuerte@op.pl>
2018-01-21 11:51:43 +0100

    project initial structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
-class App extends Component {
+import Home from './Home'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import Register from './components/Register'
+import Header from './Header'
+import Footer from './Footer'
+
+import {Grid} from 'react-bootstrap'
+
+class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Witaj! Valor react app...</h1>
-      </div>
-    );
+      <BrowserRouter>
+        <Grid>
+          <Header/>
+
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/register" component={Register}/>
+            <Route path="/profile" component={Profile}/>
+          </Switch>
+
+          <Footer/>
+        </Grid>
+      </BrowserRouter>
+
+    )
   }
 }
+
 
 export default App;
