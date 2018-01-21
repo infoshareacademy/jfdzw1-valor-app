9c3f8b4dcd23ec24e1c7f6ef5c0df7d9b4529f8b
Krzysztof <lamuerte@op.pl>
2018-01-21 12:41:51 +0100

    header and footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,32 @@
 import React  from 'react'
+import { NavLink } from "react-router-dom";
 
 class Footer extends React.Component {
   render() {
     return (
-      <h1>Footer</h1>
+      <div>
+        <hr />
+
+        <ul className="list-inline">
+          <li>
+            <NavLink exact to={"/"} activeClassName="bg-success">
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink activeClassName="bg-success" to={"/login"}>
+              Zaloguj
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink activeClassName="bg-success" to={"/register"}>
+              Zarejestruj się
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,32 @@
 import React  from 'react'
+import { Navbar, NavItem, MenuItem, Nav, NavDropdown } from "react-bootstrap";
+import {LinkContainer} from 'react-router-bootstrap'
 
 class Header extends React.Component {
   render() {
     return (
-      <h1>Header</h1>
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>Valor</Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer exact to={"/"}>
+              <NavItem>Home</NavItem>
+            </LinkContainer>
+
+            <LinkContainer to={"/login"}>
+              <NavItem>Zaloguj</NavItem>
+            </LinkContainer>
+
+            <LinkContainer to={"/register"}>
+              <NavItem>Zarejestruj się</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
