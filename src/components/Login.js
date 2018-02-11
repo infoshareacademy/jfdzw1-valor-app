d208e23fd32d22d34f15163165c82d48412be7ff
Krzysztof <lamuerte@op.pl>
2018-02-11 12:30:08 +0100

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,28 @@
-import React  from 'react'
+import React from 'react'
+import { Col, Form, FormGroup, FormControl, Checkbox, Button, ControlLabel } from "react-bootstrap";
 
 class Login extends React.Component {
   render() {
     return (
-      <h1>Login</h1>
+      <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Login
+          </Col>
+          <Col sm={10}>
+            <FormControl type="input" placeholder="Login"/>
+          </Col>
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Hasło
+          </Col>
+          <Col sm={10}>
+            <FormControl type="password" placeholder="Hasło"/>
+          </Col>
+        </FormGroup>
+      </Form>
     )
   }
 }
